3e8b3d28cd342de382c51662c7b7340ac1239da9
ChristopherStephan <ChristopherStephan@users.noreply.github.com>
2014-03-01 16:23:51 +0100

    Update snapObservationsToOsmRoads.R

--- snapObservationsToOsmRoads/snapObservationsToOsmRoads.R
+++ snapObservationsToOsmRoads/snapObservationsToOsmRoads.R
@@ -118,40 +118,6 @@ mySnapPointsToLines <- function( points, lines, maxDist=NA, withAttrs=TRUE) {
   print(df)
   SpatialPointsDataFrame(coords = t(mNewCoords), data = df, proj4string = CRS(proj4string(points)))
   
-  #
-  require("rgeos")
-  
-  if (!is.na(maxDist)) {
-    w = gWithinDistance(points, lines, dist=maxDist, byid=TRUE)
-    validPoints = apply(w,2,any)
-    validLines = apply(w,1,any)
-    points = points[validPoints,]
-    lines =  lines[validLines,]
-  }
-  
-  d = gDistance(points, lines, byid=TRUE) 
-  nearest_line_index = apply(d, 2, which.min) # Position of each nearest line in lines object 
-  
-  coordsLines = coordinates(lines)  
-  coordsPoints = coordinates(points)  
-  
-  # Get coordinates of nearest points lying on nearest lines
-  mNewCoords = vapply(1:length(points), 
-                      function(x) 
-                        nearestPointOnLine(coordsLines[[nearest_line_index[x]]][[1]], 
-                                           coordsPoints[x,]), FUN.VALUE=c(0,0))
-  
-  # Recover lines' Ids (Ids and index differ if maxDist is given)
-  nearest_line_id = lines@data[,"id"][nearest_line_index]
-  
-  
-  # Create data frame and sp points
-  if (withAttrs) df = cbind(points@data, nearest_line_id) 
-  else df = data.frame(nearest_line_id, row.names=names(nearest_line_index))
-  
-  SpatialPointsDataFrame(coords=t(mNewCoords), data=df, 
-                         proj4string=CRS(proj4string(points)))
-  
 }
 
 require ("osmar")
