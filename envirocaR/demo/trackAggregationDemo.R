82e583640e8f35ebfff6ca994dfb173e9446b1d4
Christoph Stasch <staschc@uni-muenster.de>
2014-03-06 11:24:11 +0100

    plotting of time series fixed

--- envirocaR/demo/trackAggregationDemo.R
+++ envirocaR/demo/trackAggregationDemo.R
@@ -11,6 +11,7 @@ randomId = trackIds[sample(1:size,1)]
 trackData = importSingleTrack(serverUrl,randomId)
 class(trackData)
 
+?aggregateTrack
 newTrack = aggregateTrack(track=trackData,phen="Speed",interval=10,fn=mean)
 class(newTrack)
 
@@ -21,7 +22,8 @@ plot(trackData@tracks$Track1@sp@coords,col="blue",pch="*")
 points(newTrack@sp,col="red",pch="o")
 points(newTrack2@sp,col="green",pch="O")
 
-plot(trackData@tracks$Track1@data$Speed,type="l",col="blue"); par(new=TRUE)
-plot(newTrack@data$aggr.Speed,type="l",col="red",lwd=2); par(new=TRUE)
-plot(newTrack2@data$Speed,type="l",col="green",lwd=2)
+plot(trackData@tracks$Track1@data$time,trackData@tracks$Track1@data$Speed,type="l",col="blue",ylab="Speed",xlab="Minutes");
+lines(newTrack@data$time,newTrack@data$aggr.Speed,col="red")
+lines(newTrack2@data$time,newTrack2@data$Speed,col="green")
+
 
