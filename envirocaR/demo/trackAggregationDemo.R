f95e5637691959c6b8d3bf5af632d3991e69e5f1
Christoph Stasch <staschc@uni-muenster.de>
2014-03-14 11:33:40 +0100

    cleaned
    1. removed envirocaR subfolder
    2. put aggregateTrack function into seperate R script
    3. added .gitignore file

--- envirocaR/demo/trackAggregationDemo.R
+++ envirocaR/demo/trackAggregationDemo.R
@@ -1,27 +1,25 @@
 library(envirocaR)
-serverUrlAndID = "https://envirocar.org/api/dev/tracks/51d333eae4b01cbb274b7073"
+
+#query track IDs and choose random track
 serverUrl = "https://envirocar.org/api/stable"
-trackID = "51d333eae4b01cbb274b7073"
-trackData = importSingleTrack(serverUrl,trackID)
-
 trackIds = getTrackIDs(serverUrl)
-size = length(trackIds)
-randomId = trackIds[sample(1:size,1)]
+randomId = trackIds[sample(1:length(trackIds),1)]
 
+#import track 
 trackData = importSingleTrack(serverUrl,randomId)
-class(trackData)
 
-?aggregateTrack
+#aggregate to average speed of tracks over groups of ten measurements
 newTrack = aggregateTrack(track=trackData,phen="Speed",interval=10,fn=mean)
-class(newTrack)
 
+#aggregate with default values
 newTrack2 = aggregateTrack(track=trackData)
-class(newTrack2)
 
+#plot points which are chosen
 plot(trackData@tracks$Track1@sp@coords,col="blue",pch="*")
 points(newTrack@sp,col="red",pch="o")
 points(newTrack2@sp,col="green",pch="O")
 
+#plot 
 plot(trackData@tracks$Track1@data$time,trackData@tracks$Track1@data$Speed,type="l",col="blue",ylab="Speed",xlab="Minutes");
 lines(newTrack@data$time,newTrack@data$aggr.Speed,col="red")
 lines(newTrack2@data$time,newTrack2@data$Speed,col="green")
